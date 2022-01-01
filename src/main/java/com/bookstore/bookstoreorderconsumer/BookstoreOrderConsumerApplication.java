c2c42255d4cad2f5acc6b5f63926b031c5b931fe
Fatih Buğra ÇETİNKAYA <fbugra@gmail.com>
2022-01-01 14:59:57 +0300

    Fist Commit

---
+++ src/main/java/com/bookstore/bookstoreorderconsumer/BookstoreOrderConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookstoreorderconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreOrderConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreOrderConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookstore/bookstoreorderconsumer/BookstoreOrderConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookstoreorderconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreOrderConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
